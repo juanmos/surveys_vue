2495efcc9951739bcdbe63d0d218913f2ae6b3f0
Juan M. Garcia <garcia.solutions@gmail.com>
2018-08-16 17:38:33 -0500

    initializing enviroment

---
+++ src/router.js
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+const Login = resolve => {
+  require.ensure(['./views/Login.vue'], () => {
+    resolve(require('./views/Login.vue'))
+  }, 'login')
+}
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: 'home',
+      component: Home,
+      children: []
+    },
+    {
+      path: '/pages',
+      redirect: '/pages/404',
+      name: 'Pages',
+      component: {
+        render (c) { return c('router-view') }
+      },
+      children: [
+        {
+          path: 'login',
+          name: 'Login',
+          component: Login
+        }
+      ]
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-flex xs12 sm6 offset-sm3>
+            <v-card>
+                <v-container
+                  fluid
+                  grid-list-lg
+                >
+                    <login-form></login-form>
+                </v-container>
+            </v-card>
+        </v-flex>
+    </div>
+</template>
+
+<script>
+import LoginForm from './../components/forms/LoginForm'
+export default {
+  components: {LoginForm}
+}
+</script>
+
+<style>
+
+</style>
