3c5cc54bf4131e14a02ff24d5b2c9b7f63dca514
cborja <cborja@lynxtrade.com>
2018-09-03 17:46:44 -0500

    agregando enviroment

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -2,7 +2,7 @@ import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
-var enviroment = require('./../config/enviroment')
+const enviroment = require('./../config/enviroment')
 const socket = io(enviroment[enviroment.currentEnviroment].backend.urlBase, {transports: ['websocket']})
 const feathersClient = feathers()
   .configure(socketio(socket))
