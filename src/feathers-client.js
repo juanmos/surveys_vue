def63c1c993b4b9797bedaa441a0e65321eac250
cborja <cborja@lynxtrade.com>
2018-09-03 17:57:49 -0500

    agregando dev

--- src/feathers-client.js
+++ src/feathers-client.js
@@ -3,12 +3,8 @@ import socketio from '@feathersjs/socketio-client'
 import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
 const enviroment = require('./../config/enviroment')
-const socket = io(enviroment[enviroment.currentEnviroment].backend.urlBase, {
-  transports: ['websocket']
-})
+const socket = io(enviroment[enviroment.currentEnviroment].backend.urlBase, {transports: ['websocket']})
 const feathersClient = feathers()
   .configure(socketio(socket))
-  .configure(auth({
-    storage: window.localStorage
-  }))
+  .configure(auth({ storage: window.localStorage }))
 export default feathersClient
