2163261660d1c06073f54cbf06a12e9770e92837
cborja <cborja@lynxtrade.com>
2018-09-10 18:03:38 -0500

    kanban refactor detail

--- src/views/boards/BoardsDetail.vue
+++ src/views/boards/BoardsDetail.vue
@@ -36,7 +36,8 @@
 <script>
 import {mapState, mapGetters, mapActions} from 'vuex'
 import BoardsCategories from './BoardsCategories'
-import NodeDiagram from './../mindMaps/kanban/KanbanDiagram'
+import NodeDiagram from './../mindMaps/NodeDiagram'
+import KanbanDiagram from './../mindMaps/kanban/KanbanDiagram'
 export default {
   data () {
     return {
@@ -64,7 +65,7 @@ export default {
       console.log(boards)
     })
   },
-  components: {BoardsCategories, NodeDiagram}
+  components: {BoardsCategories, NodeDiagram, KanbanDiagram}
 }
 </script>
 
