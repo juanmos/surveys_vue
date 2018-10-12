5f935d8f752dde70672ee2144bf030ccdbff3ca9
Juan Carlos <juankurdiales@hotmail.com>
2018-10-12 16:06:20 -0500

    Crud para las categorias de las encuestas

---
+++ src/views/category-projects/CProjectForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-card>
+      <v-container
+        fluid
+        grid-list-lg
+      >
+        <v-layout row wrap>
+          <v-flex xs12>
+            <v-form
+            v-model="valid"
+            @submit.prevent="sendData"
+            @keydown.prevent.enter
+            >
+                <v-text-field
+                  v-model="category.name"
+                  :rules="rules.nameRules"
+                  label="Nombre"
+                  box
+                  color="blue-grey lighten-2"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  v-model="category.description"
+                  label="Descripcion"
+                  box
+                  color="blue-grey lighten-2"
+                  required
+                ></v-text-field>
+                <v-btn type="submit" :disabled="!valid"  small color="info">Guardar</v-btn>
+            </v-form>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+</template>
+
+<script>
+import {validations} from './../../utils/validations'
+export default {
+  data: (vm) => ({
+    category: {
+      name: '',
+      description: '',
+      removed: false
+    },
+    valid: false,
+    rules: validations
+  }),
+  methods: {
+    sendData () {
+      if (this.valid) {
+        this.$emit('dataSubmited', this.category)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
