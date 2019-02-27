158aae05be21398235962d542bd0f9d52a9c6f4d
cborja <cborja@lynxtrade.com>
2019-02-27 15:52:34 -0500

    preguntas relacionadas

--- src/views/results-view/PollResultsTable.vue
+++ src/views/results-view/PollResultsTable.vue
@@ -13,6 +13,26 @@
                     </span>
                     <span v-else>{{ props.item[key] }}</span>
                 </td>
+                <td class="justify-center layout px-0" v-if="variablesMode">
+                  <v-menu
+                    bottom
+                    transition="slide-y-transition"
+                  >
+                    <v-btn
+                      slot="activator"
+                      color="primary"
+                      flat
+                      icon
+                    >
+                    <v-icon>more_vert</v-icon>
+                    </v-btn>
+                    <v-list>
+                      <v-list-tile @click="dialogRelated = true; currentEdit = props.item[key]; fieldSelected = key; arrIndex = props.index">
+                          <v-list-tile-title>Agregar relacionados</v-list-tile-title>
+                      </v-list-tile>
+                    </v-list>
+                  </v-menu>
+                </td>
             </template>
         </v-data-table>
         <v-dialog
@@ -23,17 +43,28 @@
 
           <labels-poll-editor @saveValue="editVariables" @close="editLabelDialog = false" :value="currentEdit" v-else></labels-poll-editor>
         </v-dialog>
+
+        <v-dialog v-model="dialogRelated" max-width="900">
+          <v-card v-if="dialogRelated">
+            <v-flex xs12 style="background:#d9323a;color:white;height:45px;padding: 12px;">
+              <h4 for="">Relacionar pregunta</h4>
+            </v-flex>
+            <related-question :arrIndex="arrIndex" :responses="responses" :currentPoll="currentPoll" @close="dialogRelated = false"></related-question>
+          </v-card>
+        </v-dialog>
     </div>
 </template>
 
 <script>
 import LabelsEditor from './../../components/forms/LabelsEditor'
 import LabelsPollEditor from './../../components/forms/LabelsPollEditor'
+import RelatedQuestion from './RelatedQuestion'
 export default {
   props: ['responses', 'headers', 'variablesMode', 'currentPoll'],
   data () {
     return {
       editLabelDialog: false,
+      dialogRelated: false,
       currentEdit: null,
       fieldSelected: '',
       arrIndex: null
@@ -53,7 +84,7 @@ export default {
       this.editLabelDialog = false
     }
   },
-  components: { LabelsEditor, LabelsPollEditor }
+  components: { LabelsEditor, LabelsPollEditor, RelatedQuestion }
 }
 </script>
 
