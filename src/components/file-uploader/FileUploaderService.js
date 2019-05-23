238af5ea701f7073c226bc68503cbe06d33d04b3
Jnk8s <juan.urdiales@propraxis.com.ec>
2019-05-23 17:19:13 -0500

    eliminado logs

--- src/components/file-uploader/FileUploaderService.js
+++ src/components/file-uploader/FileUploaderService.js
@@ -1,7 +1,6 @@
 import axios from './../../endpoints/standart.js'
 
 function upload (formData) {
-  console.log('lo que envio al uploads')
   const url = `/uploads`
   return axios.post(url, formData, { headers: {
     'Content-Type': 'multipart/form-data'
