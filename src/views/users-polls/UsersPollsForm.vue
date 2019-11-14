a9fbad98e0f62f0994382ca8e903fd9e42ec2c7d
cborja <cborja@lynxtrade.com>
2019-11-14 15:03:15 -0500

    add users

--- src/views/users-polls/UsersPollsForm.vue
+++ src/views/users-polls/UsersPollsForm.vue
@@ -46,6 +46,12 @@
                 v-model="userPolls._rol_id" label="Seleccione rol:"
                 ></v-autocomplete>
 
+                <v-autocomplete v-bind:items="getCompanies"
+                item-text="name"
+                item-value="_id"
+                v-model="userPolls._company_id" label="Empresa:"
+                ></v-autocomplete>
+
               <v-text-field
                   :append-icon="showPass ? 'visibility_off' : 'visibility'"
                   v-model="userPolls.password"
@@ -83,6 +89,7 @@ export default {
       email: '',
       cedula: '',
       password: null,
+      _companay_id: null,
       confirmPassword: null,
       phones: '',
       _rol_id: null,
@@ -98,6 +105,7 @@ export default {
   }),
   methods: {
     ...mapActions('roles', { findRoles: 'find' }),
+    ...mapActions('companies', { findCompanies: 'find' }),
     sendData () {
       if (this.valid) {
         this.$emit('dataSubmited', this.userPolls)
@@ -113,8 +121,12 @@ export default {
   computed: {
     ...mapState('roles', {loading: 'isFindPending'}),
     ...mapGetters('roles', {findRolesInStore: 'find'}),
+    ...mapGetters('companies', {findCompaniesInStore: 'find'}),
     getRoles () {
-      return this.findRolesInStore({query: {removed: false, $skip: this.getSkip, $limit: this.limit, ...this.query}}).data
+      return this.findRolesInStore({query: {removed: false, ...this.query}}).data
+    },
+    getCompanies () {
+      return this.findCompaniesInStore({query: {removed: false, ...this.query}}).data
     }
   },
   watch: {
@@ -126,12 +138,8 @@ export default {
     }
   },
   created () {
-    this.findRoles({query: {removed: false, $skip: this.getSkip, $limit: this.limit, ...this.query}}).then(response => {
-      this.limit = response.limit
-      this.total = response.total
-      this.loaded = true
-      this.roles = response.data
-    })
+    this.findRoles({query: {removed: false, $skip: 0, $limit: null, ...this.query}})
+    this.findCompanies({query: {removed: false, $skip: 0, $limit: null, ...this.query}})
   }
 }
 </script>
