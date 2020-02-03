8b0520dfe6669c1328690f3593d8ef535ed1192d
cborja <cborja@lynxtrade.com>
2020-02-03 10:07:53 -0500

    validar que exista dato

--- src/views/polls-project/PollsView.vue
+++ src/views/polls-project/PollsView.vue
@@ -762,6 +762,7 @@ export default {
     },
     getConfigPolls () {
       this.findConfigPolls({query: {$sort: { dateFinished: '-1' }, removed: false, _polls_project_id: this.$route.params.id}}).then(response => {
+        console.log('res--', response)
         this.listConfigPolls = response.data
       })
     },
